ba403d0e554aad1b3d1e6c092b6d8801f653135d
David Anderson <david@folklogic.com>
2010-02-03 08:32:45 +0100

    made the user_id mechanism thread-safe

--- lib/logjam_logger/formatter.rb
+++ lib/logjam_logger/formatter.rb
@@ -6,12 +6,12 @@ module LogjamLogger
 
     private
 
-    # Logging user_ids via a global variable, $user_id.
+    # Logging user_ids via a global hash, $user_ids.
     # You can use a before_filter in your application_controller to set it, eg:
     #
-    #    before_filter { |controller| $user_id = controller.session[:user_id] || 0 }
+    #    before_filter { |controller| ($user_ids ||= {})[Thread.current] = controller.session[:user_id] || 0 }
     def user_id
-      defined?($user_id) ? " user[#{$user_id}]" : ''
+      defined?($user_ids) ? " user[#{$user_ids[Thread.current]}]" : ''
     end
 
     # Note: If you are using FastCGI you may need to hard-code the hostname here instead of using Socket.gethostname
